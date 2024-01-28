3888c9087b87c47c9105286b9ccc6c6a13f6d0d5
jua12002 <jua12002@byui.edu>
2024-01-28 09:41:40 -0600

    last

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -13,21 +13,19 @@
 use('mongodbVSCodePlaygroundDB');
 
 // Insert a few documents into the sales collection.
-db.getCollection('sales').insertMany([
-  { 'item': 'abc', 'price': 10, 'quantity': 2, 'date': new Date('2014-03-01T08:00:00Z') },
-  { 'item': 'jkl', 'price': 20, 'quantity': 1, 'date': new Date('2014-03-01T09:00:00Z') },
-  { 'item': 'xyz', 'price': 5, 'quantity': 10, 'date': new Date('2014-03-15T09:00:00Z') },
-  { 'item': 'xyz', 'price': 5, 'quantity': 20, 'date': new Date('2014-04-04T11:21:39.736Z') },
-  { 'item': 'abc', 'price': 10, 'quantity': 10, 'date': new Date('2014-04-04T21:23:13.331Z') },
-  { 'item': 'def', 'price': 7.5, 'quantity': 5, 'date': new Date('2015-06-04T05:08:13Z') },
-  { 'item': 'def', 'price': 7.5, 'quantity': 10, 'date': new Date('2015-09-10T08:43:00Z') },
-  { 'item': 'abc', 'price': 10, 'quantity': 5, 'date': new Date('2016-02-06T20:20:13Z') },
+db.getCollection('contacts').insertMany([
+  { firstName: 'Faby', lastName: 'Juz',   email: '2@gmail.com', favoriteColor: 'Blue', date: new Date('2014-03-17') },
+  { firstName: 'Kim', lastName: 'Sac',   email: '3@gmail.com', favoriteColor: 'Grey', date: new Date('2014-03-16') },
+  { firstName: 'Bear', lastName: 'Saj',   email: '1@gmail.com', favoriteColor: 'Red', date: new Date('2014-03-23') }
 ]);
 
 // Run a find command to view items sold on April 4th, 2014.
-const salesOnApril4th = db.getCollection('sales').find({
-  date: { $gte: new Date('2014-04-04'), $lt: new Date('2014-04-05') }
-}).count();
+const salesOnApril4th = db
+  .getCollection('contacts')
+  .find({
+    date: { $gte: new Date('2014-04-04'), $lt: new Date('2014-04-05') }
+  })
+  .count();
 
 // Print a message to the output window.
 console.log(`${salesOnApril4th} sales occurred in 2014.`);
@@ -35,9 +33,9 @@ console.log(`${salesOnApril4th} sales occurred in 2014.`);
 // Here we run an aggregation and open a cursor to the results.
 // Use '.toArray()' to exhaust the cursor to return the whole result set.
 // You can use '.hasNext()/.next()' to iterate through the cursor page by page.
-db.getCollection('sales').aggregate([
+db.getCollection('contacts').aggregate([
   // Find all of the sales that occurred in 2014.
   { $match: { date: { $gte: new Date('2014-01-01'), $lt: new Date('2015-01-01') } } },
   // Group the total sales for each product.
-  { $group: { _id: '$item', totalSaleAmount: { $sum: { $multiply: [ '$price', '$quantity' ] } } } }
+  { $group: { _id: '$item', totalSaleAmount: { $sum: { $multiply: ['$price', '$quantity'] } } } }
 ]);
